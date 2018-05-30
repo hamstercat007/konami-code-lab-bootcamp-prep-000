d962e3ea63529ae7c43bdcd41c75c20c32cf5c35
Lisa Mac <lisamac421@gmail.com>
2018-05-30 21:45:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,6 +14,7 @@ const codes = [
 let index = 0;
 function init() {
   document.body.addEventListener("keydown", function onKeydownHandler(e) {
+    console.log('cat')
     if (codes[index] === e.key) {
       console.log(e.key + " is pressed correctly")
       index++
